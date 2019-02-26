c25b8152f3a931723680b58012c31485aea490bc
zhujian521 <1179758791@qq.com>
2019-02-26 15:56:12 +0800

    add time tool

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var timeUtils_1 = require("./timeUtils");
+function add(a, b) {
+    return a + b;
+}
+exports.add = add;
+console.log(timeUtils_1.TimeUtilsModule.getCurrentTime());
